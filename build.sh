49211bd3d59eeddc5622d38289237ed7200a8604
mc_fdc <Masato.04.11.2007@gmail.com>
2022-12-24 10:04:45 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 wget https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_5340.zip
 unzip -q kagglecatsanddogs_5340.zip
 
-pip install tensorflow-cpu pillow tf2onnx tensorflow_hub
+pip install tensorflow-cpu pillow tf2onnx tensorflow_hub scipy
